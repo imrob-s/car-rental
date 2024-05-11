20b448fd7aeccfd38c62a5e095300f7bbff699dd
imrob-s <orobsilva@gmail.com>
2024-05-11 16:49:42 -0300

    Project created

---
+++ backend/src/main/java/dev/imrob/apilocadoraveiculos/ApiLocadoraVeiculosApplication.java
@@ -0,0 +1,13 @@
+package dev.imrob.apilocadoraveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLocadoraVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLocadoraVeiculosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/imrob/apilocadoraveiculos/ApiLocadoraVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.imrob.apilocadoraveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLocadoraVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
